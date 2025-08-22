832a9d1d5b293bb250fe1450075b9513af98ba15
ChPuru <puruchoudhary9b@gmail.com>
2025-08-22 16:27:35 +0530

    more changes

--- .config/ags/config.js
+++ .config/ags/config.js
@@ -1,9 +1,29 @@
+// ~/.config/ags/config.js
 import { bar } from './modules/bar.js';
 import { dashboard } from './modules/dashboard.js';
-import { control_center } from './modules/control-center.js';
-import { launcher } from './modules/launcher.js'; // NEW
+import { launcher } from './modules/launcher.js';
+import { sidebar } from './modules/sidebar.js';
+import { overview } from './modules/overview.js';
+import { notifications } from './modules/notifications.js';
+import { calendar } from './modules/calendar.js';
+import { music } from './modules/music.js';
+import { system } from './modules/system.js';
 
 App.config({
     style: './style.css',
-    windows: [ bar, dashboard, control_center, launcher ], // NEW
+    windows: [
+        bar,
+        dashboard,
+        launcher,
+        sidebar,
+        overview,
+        notifications,
+        calendar,
+        music,
+        system
+    ],
+    closeWindowDelay: {
+        'launcher': 500,
+        'dashboard': 500,
+    },
 });
